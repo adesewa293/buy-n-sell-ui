5be9d6a37c31ca3dea6ee042ee2b071a8e8668a4
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-16 03:36:23 +0000

    add featured product section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,24 @@
-import Header from "./components/Header"
+import { mongooseConnect } from "@/lib/mongoose";
+import Featured from "../components/Featured"
+import Header from "../components/Header"
+import { Product } from "@/models/product";
 
 
+async function getProduct(){
+  const featuredProductId = "65dfe49d894e08e55f0653c8";
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return JSON.parse(JSON.stringify(product));
+ }
 
-export default function Homepage() {
+
+export default async function page() {
+  const product = await getProduct();
+  console.log('product', product);
   return (
     <div>
       <Header />
+      <Featured product={product}/>
     </div>
   )
 }
