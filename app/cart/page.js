312746aa5ca3373fdd9b25a7e120971c4b34e43f
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-18 17:13:48 +0000

    update add-to-cart-feature

--- app/cart/page.js
+++ app/cart/page.js
@@ -7,6 +7,7 @@ import Button from "@/components/Button";
 import { useContext, useEffect, useState } from "react";
 import { CartContext } from "@/components/CartContext";
 import axios from "axios";
+import Table from "@/components/Table";
 
 const ColumnsWrapper = styled.div`
   display: grid;
@@ -20,19 +21,54 @@ const Box = styled.div`
   border-radius: 10px;
   padding: 30px;
 `;
+const ProductInfoCell = styled.td`
+  padding: 10px 0;
+`;
+const ProductImageBox = styled.div`
+  width: 100px;
+  height: 100px;
+  padding: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  img {
+    max-width: 80px;
+    max-height: 80px;
+  }
+`;
+
+const QuantityLabel = styled.span`
+  padding: 0 3px;
+`;
 
 export default function CartPage() {
-  const { cartProducts } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
   const [products, setProducts] = useState([]);
 
+  console.log(cartProducts.length, products.length);
   useEffect(() => {
     if (cartProducts.length > 0) {
       axios.post("api/cart", { ids: cartProducts }).then((response) => {
         setProducts(response.data);
-        console.log('response.data', response.data)
+        console.log("response.data", response.data);
       });
     }
   }, [cartProducts]);
+  function moreOfThisProduct(id) {
+    addProduct(id);
+  }
+
+  function lessOfThisProduct(id) {
+    removeProduct(id);
+  }
+
+  let total = 0;
+  for (const productId of cartProducts) {
+    const price = products.find(p => p._id === productId)?.price || 0;
+    total += price;
+  } 
   return (
     <>
       <Header />
@@ -40,19 +76,57 @@ export default function CartPage() {
       <Center>
         <ColumnsWrapper>
           <Box>
-            {!cartProducts?.length && <div>Your cart is empty</div>}
+            <h2>Cart</h2>
 
+            {!cartProducts?.length && <div>Your cart is empty</div>}
             {products?.length > 0 && (
-              <>
-                <h2>Cart</h2>
-                {products.map((product) => (
-                  <div>{product.title}</div>
-                ))}
-                
-              </>
-              
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Products</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr>
+                      <ProductInfoCell>
+                        <ProductImageBox>
+                          {" "}
+                          <img src={product.images[0]} alt="" />{" "}
+                        </ProductImageBox>
+                        {product.title}
+                      </ProductInfoCell>
+                      <td>
+                        <button onClick={() => lessOfThisProduct(product._id)}>
+                          -
+                        </button>
+                        <QuantityLabel>
+                          {
+                            cartProducts.filter((id) => id === product._id)
+                              .length
+                          }
+                        </QuantityLabel>
+                        <button onClick={() => moreOfThisProduct(product._id)}>
+                          +
+                        </button>
+                      </td>
+                      <td>
+                        $
+                        {cartProducts.filter((id) => id === product._id)
+                          .length * product.price}
+                      </td>
+                    </tr>
+                  ))}
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td>${total}</td>
+                  </tr>
+                </tbody>
+              </Table>
             )}
-            
           </Box>
           {!!cartProducts?.length && (
             <Box>
