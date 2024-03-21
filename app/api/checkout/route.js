dc466fff7931d823a0cd3feafbc1cc6fa1a9bb8b
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-21 23:45:08 +0000

    update stripe checkout feature

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,39 +1,66 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/Order";
-import {Product} from '@/models/Product'
-const stripe = require('stripe')('sk_test_...');
+import { Product } from "@/models/Product";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { NextResponse } from "next/server";
 
-
-export async function POST(req,res) {
- if (req.method !== 'POST'){
-  res.json('should be a post request');
-  return;
- }
- const {name, email, city, postalCode, streetAddress, country, products} = req.body;
- await mongooseConnect();
- const productIds = products.split(',');
- const uniqueIds =[...new Set(productIds)];
- const productInfos =await Product.find({_id:uniqueIds});
-
-
- let line_items =[];
- for (const productId of uniqueIds){
-  const productInfo = productInfos.find(p => p._id.toString() === productId);
-  const quantity = productIds.filter(id => id === productId)?.length || 0;
-  if (quantity > 0 && productInfo) {
-    line_items.push({
-      quantity,
-      price_data: {
-          currency: 'USD',
-          product_data: {name:productInfo.title},
-          unit_amount: quantity * productInfo.price,
-      }
-    })
+export async function POST(req, res) {
+  if (req.method !== "POST") {
+    // res.json("should be a post request");
+    return;
   }
- 
- }
-const orderDoc = await Order.create({
-  line_items, name, email, city, postalCode, streetAddress, country, paid:false
-});
+  const formData = await req.formData();
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const city = formData.get("city");
+  const postalCode = formData.get("postalCode");
+  const streetAddress = formData.get("streetAddress");
+  const country = formData.get("country");
+  const products = formData.get("products");
+
+  console.log('formData', formData, products);
+  await mongooseConnect();
+  const productIds = products.split(",");
+  const uniqueIds = [...new Set(productIds)];
+  const productInfos = await Product.find({ _id: uniqueIds });
+
+  let line_items = [];
+  for (const productId of uniqueIds) {
+    const productInfo = productInfos.find(
+      (p) => p._id.toString() === productId
+    );
+    const quantity = productIds.filter((id) => id === productId)?.length || 0;
+    if (quantity > 0 && productInfo) {
+      line_items.push({
+        quantity,
+        price_data: {
+          currency: "USD",
+          product_data: { name: productInfo.title },
+          unit_amount: quantity * productInfo.price * 100,
+        },
+      });
+    }
+  }
+  const orderDoc = await Order.create({
+    line_items,
+    name,
+    email,
+    city,
+    postalCode,
+    streetAddress,
+    country,
+    paid: false,
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: 'payment',
+    customer_email: email,
+    success_url: process.env.PUBLIC_URL + '/cart?success=1',
+    cancel_url: process.env.PUBLIC_URL + '/cart?canceled=1',
+    metadata: {orderId:orderDoc._id.toString()}
+  })
+
+  return NextResponse.json({url:session.url});
+
 
 }
--- models/Order.js
+++ models/Order.js
@@ -1,15 +1,14 @@
 const { Schema, model, models } = require("mongoose");
 
 const OrderSchema = new Schema({
-line_items:Object,
-name:String,
-email:String,
-city:String,
-postalCode:String,
-streetAddress: String,
-country: String,
-paid:Boolean,
+  line_items: Object,
+  name: String,
+  email: String,
+  city: String,
+  postalCode: String,
+  streetAddress: String,
+  country: String,
+  paid: Boolean,
 });
 
-
-export const Order = models?.order || model('Order', OrderSchema);
+export const Order = models?.Order || model("Order", OrderSchema);
