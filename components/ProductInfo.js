bf6d611284f18c40a1c4835ba8777c87f97456e7
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-26 04:17:18 +0000

    update individual product information page

--- components/ProductInfo.js
+++ components/ProductInfo.js
@@ -2,13 +2,47 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import Title from "@/components/Title";
+import styled from "styled-components";
+import WhiteBox from "./WhiteBox";
+import ProductImages from "./ProductImages";
+import Button from "./Button";
+import CartIcon from "@/icons/CartIcon";
+
+const ColWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 0.8fr 1.2fr;
+  gap: 40px;
+  margin-top: 40px;
+`;
+const PriceRow = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
 
 export default function ProductInfo({ product }) {
   return (
     <>
       <Header />
       <Center>
-        <Title>{product.title}</Title>
+        <ColWrapper>
+          <WhiteBox>
+            <ProductImages images={product.images} />
+          </WhiteBox>
+          <div>
+            <Title>{product.title}</Title>
+            <p>{product.description}</p>
+            <PriceRow>
+              <div>${product.price}</div>
+              <div>
+                <Button $primary>
+                  <CartIcon />
+                  Add to cart
+                </Button>
+              </div>
+            </PriceRow>
+          </div>
+        </ColWrapper>
       </Center>
     </>
   );
--- components/ProductsGrid.js
+++ components/ProductsGrid.js
@@ -13,7 +13,7 @@ export default function ProductsGrid({products}) {
   return (
     <StyledProductsGrid>
       {products?.length > 0 && products.map(product => (
-        <ProductBox {...product} />
+        <ProductBox key={product._id}{...product} />
       ))
       }
     </StyledProductsGrid>
