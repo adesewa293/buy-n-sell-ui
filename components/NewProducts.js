d58e63a7582d33f1b6058ee7d6c381ad8109ff8b
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-27 21:52:16 +0000

    update mobile-responsiveness

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -6,17 +6,19 @@ import ProductBox from "./ProductBox";
 
 const ProductsGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+
+  @media screen and (min-width: 768px){
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  
+  }
   gap: 20px;
 `;
 
 const Title = styled.h2`
 font-size: 2rem;
 margin: 30px 0 20px;
-
 `;
-
-
 
 export default function NewProducts({ products }) {
   return (
